ac99d11a40bce77764206400a8af1a2c6f1cea5f
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-08-13 09:53:10 +0700

    build paginate plugin

---
+++ models/plugins/paginate.plugin.ts
@@ -0,0 +1,64 @@
+import { Schema, Document, Model, PopulateOptions } from 'mongoose';
+
+interface PaginateOptions {
+  sortBy?: string; // e.g., 'fieldName:asc,anotherField:desc'
+  populate?: string; // e.g., 'fieldName.nestedField'
+  limit?: number;
+  page?: number;
+}
+
+interface QueryResult<T> {
+  results: T[];
+  page: number;
+  limit: number;
+  totalPages: number;
+  totalResults: number;
+}
+
+const paginate = <T extends Document>(schema: Schema<T>) => {
+  schema.statics.paginate = async function (filter: Record<string, any> = {}, options: PaginateOptions = {}): Promise<QueryResult<T>> {
+    let sort = '';
+    if (options.sortBy) {
+      const sortingCriteria: string[] = [];
+      options.sortBy.split(',').forEach((sortOption) => {
+        const [key, order] = sortOption.split(':');
+        sortingCriteria.push((order === 'desc' ? '-' : '') + key);
+      });
+      sort = sortingCriteria.join(' ');
+    } else {
+      sort = 'createdAt';
+    }
+
+    const limit = options.limit && parseInt(options.limit.toString(), 10) > 0 ? parseInt(options.limit.toString(), 10) : 10;
+    const page = options.page && parseInt(options.page.toString(), 10) > 0 ? parseInt(options.page.toString(), 10) : 1;
+    const skip = (page - 1) * limit;
+
+    const countPromise = this.countDocuments(filter).exec();
+    let docsPromise = this.find(filter).sort(sort).skip(skip).limit(limit);
+
+    if (options.populate) {
+      const populateOptions: PopulateOptions[] = options.populate.split(',').map((populateOption) => {
+        const pathParts = populateOption.split('.');
+        return pathParts.reverse().reduce<PopulateOptions>((acc, path) => ({ path, populate: acc }), {} as PopulateOptions);
+      });
+      populateOptions.forEach(populateOption => {
+        docsPromise = docsPromise.populate(populateOption);
+      });
+    }
+
+    docsPromise = docsPromise.exec();
+
+    return Promise.all([countPromise, docsPromise]).then(([totalResults, results]) => {
+      const totalPages = Math.ceil(totalResults / limit);
+      return {
+        results,
+        page,
+        limit,
+        totalPages,
+        totalResults,
+      };
+    });
+  };
+};
+
+export default paginate;
